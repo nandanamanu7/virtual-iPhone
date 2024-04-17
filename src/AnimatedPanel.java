38e8fc6e898e5d03f522636bc99a024a1cb6a142
2010844 <2010844@NCHS120490510.ms.nsd.org>
2024-04-17 09:00:09 -0700

    test Push

---
+++ src/AnimatedPanel.java
@@ -0,0 +1,15 @@
+import java.awt.color.*;
+import javax.swing.JPanel;
+
+public abstract class AnimatedPanel extends JPanel{
+	private static final long serialVersionUID = 1L;
+	
+	public AnimatedPanel() {
+		// JPanel constructor with true says to use DoubleBuffering
+		super(false);
+	}
+	
+	// All child classes must implement this method
+	public abstract void updateAnimation();
+	
+}
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,4 +1,42 @@
-public class MainFrame {
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+
+public class MainFrame extends JFrame {
 	private Screen screen = new Screen();
 	private App testApp = new InitialApp();
+	
+	public static final int WIDTH = 400;
+	public static final int HEIGHT = 800;
+	public static final int SCREEN_WIDTH = 220;
+	public static final int SCREEN_HEIGHT = 350;
+	
+	private static final int SCREEN_PANEL = 0;
+	private static final int TICTACTOE_PANEL = 1;
+	
+	private AnimatedPanel[] screens;
+	private int currentPanel = 0;
+	
+	public static int delay = 10;
+	
+	public MainFrame() {
+		
+	}
+	
+	 public static void startGUI() throws InterruptedException {
+	        MainFrame theGUI = new MainFrame();
+	        SwingUtilities.invokeLater( () -> theGUI.createFrame(theGUI) );
+	        synchronized (theGUI ) {
+	            theGUI.wait();
+	        }
+	    }
+	 
+	 public void createFrame(Object semaphore) {
+		 this.setTitle("Home Screen");
+		 this.setSize(HEIGHT,WIDTH);
+	 }
+	 
+	 
 }
