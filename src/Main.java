26261ab40fe413677d4705b3407acf03aef0b0b7
2010844 <2010844@NCHS120490510.ms.nsd.org>
2024-04-17 09:35:18 -0700

    start creation of mainframe

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
-	public static void main (String[] args) {
+	public static void main (String[] args) throws InterruptedException {
 		System.out.println("Hello");
 		MainFrame mainframe = new MainFrame();
+		mainframe.run();
 	}
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -5,38 +5,65 @@ import javax.swing.SwingUtilities;
 
 
 public class MainFrame extends JFrame {
-	private Screen screen = new Screen();
-	private App testApp = new InitialApp();
+	// To eliminate a warning showing in Eclipse
+	private static final long serialVersionUID = 1L;
 	
+	// To set bounds of the frame
 	public static final int WIDTH = 400;
 	public static final int HEIGHT = 800;
 	public static final int SCREEN_WIDTH = 220;
 	public static final int SCREEN_HEIGHT = 350;
 	
+	// Constants for each screen
 	private static final int SCREEN_PANEL = 0;
 	private static final int TICTACTOE_PANEL = 1;
 	
+	// Set up array of AnimatedPanels for each screen along with int for the current screen
 	private AnimatedPanel[] screens;
 	private int currentPanel = 0;
 	
+	// Used to establish the animation speed
 	public static int delay = 10;
 	
-	public MainFrame() {
+	public MainFrame(){
 		
 	}
 	
-	 public static void startGUI() throws InterruptedException {
-	        MainFrame theGUI = new MainFrame();
-	        SwingUtilities.invokeLater( () -> theGUI.createFrame(theGUI) );
-	        synchronized (theGUI ) {
-	            theGUI.wait();
-	        }
-	    }
+	public void run() throws InterruptedException{
+		System.out.println("this works");
+		MainFrame theGUI = new MainFrame();
+        SwingUtilities.invokeLater( () -> theGUI.createFrame(theGUI) );
+        synchronized (theGUI ) {
+            theGUI.wait();
+        }
+	}
 	 
 	 public void createFrame(Object semaphore) {
-		 this.setTitle("Home Screen");
+		 this.setTitle("Virtual iPhone");
 		 this.setSize(HEIGHT,WIDTH);
+		 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 
+	     // Set the current frame and this JFrame to be visible
+	     this.setVisible(true);
+
+	     // tell the main thread that we are done creating our stuff
+	     synchronized (semaphore) {
+	         semaphore.notify();
+	     }
 	 }
 	 
+	 private void showPanel(int index) {
+	        System.out.printf("Show Panel. Thread is: %s\n", Thread.currentThread().getName());
+
+	        // hide the current panel
+	        screens[currentPanel].setVisible(false);
+
+	        // show the correct panel
+	        currentPanel = index;
+	        screens[currentPanel].setVisible(true);
+
+	        // The animation will start on the main thread.
+	        // Do nothing in the UI thread
+	    }
 	 
 }
