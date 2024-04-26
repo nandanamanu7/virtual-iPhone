ff248df1dc6c4c4327c57715bb7bfdc657defe17
1109309 <1109309@NCHS120490509.ms.nsd.org>
2024-04-26 10:49:44 -0700

    Set up wallpaper class and images

--- src/HomeScreenPanel.java
+++ src/HomeScreenPanel.java
@@ -1,28 +1,46 @@
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-public class HomeScreenPanel extends AnimatedPanel {
+public class HomeScreenPanel extends AnimatedPanel{
 	
 	
 	private BufferedImage wallpaper;
+	private BufferedImage[] wallpapers;
+	private int currentWallpaper = 0;
 
 	
 	public HomeScreenPanel() {
 		loadImages();
+		this.wallpapers = new BufferedImage[4];
 	}
 	
 	private void loadImages() {
-		 File imX = new File("src/Images/wallpaper01.png");
-	        try {
-	            this.wallpaper = ImageIO.read(imX);
-	        } catch (IOException e) {
-	            System.out.println(imX.getAbsolutePath());
-	        }
+		 File im0 = new File("src/Images/wallpaper00.png");
+		 File im1 = new File("src/Images/wallpaper01.png");
+		 File im2 = new File("src/Images/wallpaper02.png");
+		 File im3 = new File("src/Images/wallpaper03.png");
+		 try {
+	            this.wallpapers[0] = ImageIO.read(im0);
+	            this.wallpapers[1] = ImageIO.read(im1);
+	            this.wallpapers[2] = ImageIO.read(im2);
+	            this.wallpapers[3] = ImageIO.read(im3);
+	      } catch (IOException e) {
+	            System.out.println(im0.getAbsolutePath());
+	            System.out.println(im1.getAbsolutePath());
+	            System.out.println(im2.getAbsolutePath());
+	            System.out.println(im3.getAbsolutePath());
+	      }
     }
+	
+	public void setWallpaper(int wallpaper) {
+		this.wallpaper = wallpapers[wallpaper];
+	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
---
+++ src/Wallpaper.java
@@ -0,0 +1,23 @@
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Wallpaper extends AnimatedPanel{
+	
+	@Override
+	public void updateAnimation() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void loadImages() {
+		
+	}
+
+}
+
