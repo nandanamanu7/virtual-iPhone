1487c66f5e1f560629670ecb509ad4e90e038d99
1109309 <1109309@NCHS120490509.ms.nsd.org>
2024-05-17 10:00:25 -0700

    Start of hangman

--- src/HangManPanel.java
+++ src/HangManPanel.java
@@ -1,4 +1,16 @@
 
-public class HangManPanel {
+public class HangManPanel extends AnimatedPanel{
+
+	@Override
+	public void updateAnimation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clickEvent(int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
